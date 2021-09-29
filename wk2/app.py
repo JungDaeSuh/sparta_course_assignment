e2db2af6ae9bbb6dc07420e0c820dd5d214db369
JungDaeSuh <jungdaesuh1221@gmail.com>
2021-09-29 22:22:53 +0900

    week 2-8 ajax. Before using jinja2

--- wk2/app.py
+++ wk2/app.py
@@ -1,42 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
 import requests
+
 
 app = Flask(__name__)
 
+client = MongoClient('3.34.140.192', 27017, username="test", password="test")
+db = client.dbsparta_plus_week2
+
 
 @app.route('/')
-def main():  # put application's code here
-    my_name = "Sparta"
-    return render_template('index.html', name=my_name)
+def main():
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
+    return render_template("index.html")
 
-@app.route('/detailed/<keyword>')
-def detail(keyword):  # put application's code here
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    # API에서 단어 뜻 찾아서 결과 보내기
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 605d8ee2c21155342169d23e1e421a42eb26e9d1"})
     result = r.json()
     print(result)
-    word_receive = request.args.get("word_give")
-    print(word_receive)
-    return render_template('detailed.html', word=keyword)
+    return render_template("detail.html", word=keyword, result=result)
+
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    # 단어 저장하기
+    return jsonify({'result': 'success', 'msg': '단어 저장'})
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    # 단어 삭제하기
+    return jsonify({'result': 'success', 'msg': '단어 삭제'})
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
-# from flask import Flask, render_template
-#
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/')
-# def main():
-#     return render_template("index.html")
-#
-#
-# @app.route('/detail')
-# def detail():
-#     return render_template("detailed.html")
-#
-#
-# if __name__ == '__main__':
-#     app.run('0.0.0.0', port=5000, debug=True)
