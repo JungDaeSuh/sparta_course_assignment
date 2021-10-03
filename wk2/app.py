784b27a359ebf3c98bd03098e528b1dbd7511ec6
JungDaeSuh <jungdaesuh1221@gmail.com>
2021-10-03 15:49:51 +0900

    Week 2 finished

--- wk2/app.py
+++ wk2/app.py
@@ -12,7 +12,10 @@ db = client.dbsparta_plus_week2
 @app.route('/')
 def main():
     # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    return render_template("index.html")
+    msg = request.args.get("msg")
+    words = list(db.words.find({}, {"_id": False}))
+    print(words)
+    return render_template("index.html", words=words, msg=msg)
 
 
 @app.route('/detail/<keyword>')
@@ -20,6 +23,8 @@ def detail(keyword):
     status_receive = request.args.get("status_give")
     # API에서 단어 뜻 찾아서 결과 보내기
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token 605d8ee2c21155342169d23e1e421a42eb26e9d1"})
+    if r.status_code != 200:
+        return redirect(url_for("main", msg="단어가 이상해요 ㅠㅠ"))
     result = r.json()
     print(result)
     return render_template("detail.html", word=keyword, result=result, status=status_receive)
